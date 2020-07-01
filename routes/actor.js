9874b4d8472b0bcccd6c700b4d6348be76618bc6
Daniel <eoyeniran@tradedept.co>
2020-07-01 07:47:10 +0100

    wrote function to get actor events

--- routes/actor.js
+++ routes/actor.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const jsonFile = require('jsonfile');
+const actors = require('../controllers/actors');
 const file = '/Users/daniel/Documents/projects/express-dataset/data/events.json';
 
 // Routes related to actor.
@@ -22,13 +23,8 @@ router.put('/', async function (request, response) {
 	});
 
 	data = events;
-
-	console.log(typeof data, 'type data');
-
 	let filterById = [];
 	filterById = data.filter((event) => Number(event.actor.id) === Number(id));
-
-	console.log({ filterById });
 
 	if (!filterById.length) {
 		return response.status(404).send('Event by this actor not found');
@@ -48,5 +44,128 @@ router.put('/', async function (request, response) {
 		return response.status(200).send('Updated successfully');
 	});
 });
+
+router.get('/', async function (request, response) {
+	try {
+		let data = new Array();
+		let events = await new Promise((resolve, reject) => {
+			jsonFile.readFile(file, async function (err, obj) {
+				let res = obj;
+				if (res === undefined) {
+					res = [];
+				}
+				resolve(res);
+				if (err) reject(err);
+			});
+		});
+		let finalData = new Array();
+
+		data = events;
+		const actorIds = [...new Set(data.map((e) => e.actor.id))]; //get unique actor ids
+		console.log({ actorIds });
+		actorIds.map((id) => {
+			const obj = new Object();
+			obj.id = id;
+			let actorEvents = [];
+			actorEvents = data.filter((event) => Number(event.actor.id) === Number(id));
+			obj.events = actorEvents;
+			finalData.push(obj);
+		});
+		finalData = sortByNumberOfEvents(finalData);
+		finalData = sortByCreatedAt(finalData);
+		finalData = sortByLogin(finalData);
+		return response.status(200).send({ actorsData: finalData });
+	} catch (e) {
+		console.log('here', e);
+		return response.status(400).send('Something went wrong');
+	}
+});
+
+function sortByNumberOfEvents(actorsData) {
+	try {
+		actorsData.sort((a, b) => {
+			return b.events.length - a.events.length;
+		});
+		return actorsData;
+	} catch (e) {
+		throw e;
+	}
+}
+
+function sortByCreatedAt(actorsData) {
+	actorsData.sort((a, b) => {
+		if (a.events.length === b.events.length) {
+			a.events.sort((a, b) => {
+				let fa = a.created_at;
+				fb = b.created_at;
+
+				if (fa < fb) {
+					return 1;
+				}
+				if (fa > fb) {
+					return -1;
+				}
+				return 0;
+			});
+
+			b.events.sort((a, b) => {
+				let fa = a.created_at;
+				fb = b.created_at;
+
+				if (fa < fb) {
+					return 1;
+				}
+				if (fa > fb) {
+					return -1;
+				}
+				return 0;
+			});
+		}
+	});
+	return actorsData;
+}
+
+function sortByLogin(actorsData) {
+	actorsData.sort((a, b) => {
+		if (a.events.length === b.events.length) {
+			a.events.sort((a, b) => {
+				let fa = a.created_at;
+				fb = b.created_at;
+
+				if (fb === fa) {
+					let sa = a.actor.login;
+					let sb = b.actor.login;
+
+					if (sa < sb) {
+						return 1;
+					}
+					if (sa > sb) {
+						return -1;
+					}
+					return 0;
+				}
+			});
+
+			b.events.sort((a, b) => {
+				let fa = a.created_at;
+				fb = b.created_at;
+
+				if (fb === fa) {
+					let sa = a.actor.login;
+					let sb = b.actor.login;
+
+					if (sa < sb) {
+						return 1;
+					}
+					if (sa > sb) {
+						return -1;
+					}
+					return 0;
+				}
+			});
+		}
+	});
+	return actorsData;
+}
 
 module.exports = router;
